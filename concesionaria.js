577de9d6c15e6a6d86e65b321890fa21c422c84c
lean9ema <lean9marino@gmail.com>
2021-10-19 12:30:37 -0300

    Leandro - Subida de archivos

---
+++ concesionaria.js
@@ -0,0 +1,37 @@
+const autos = require('./autos');
+
+
+const concesionaria = {
+    autos: autos,/*.forEach((autos) =>{
+    console.log(autos);})*/
+    buscarAuto(patente) {
+        let stock = this.autos
+        let autoFiltrado = stock.filter((stock) => {
+            return stock.patente == patente  
+        });
+
+        if(autoFiltrado.length > 0){
+            return autoFiltrado[0]
+        }else{
+            return null
+        }
+
+    },
+    venderAuto(patente){
+        autoFiltrado = this.buscarAuto(patente);
+        this.autos.map(function(autos){
+           autoFiltrado == autos ?  autos.vendido = true : ''
+           console.log(autos);  
+        })
+    },
+    autosParaLaVenta(){
+
+        let autosNoVendidos =this.autos.filter((stock)=>{
+            return stock.vendido != true
+        });
+        return autosNoVendidos
+    }
+}
+
+
+console.log(concesionaria.autosParaLaVenta());
